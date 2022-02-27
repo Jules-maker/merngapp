b2f0afb8d2ee0c509f8086d4798b98f500008c1e
Jules Giraud <jules.giraud@outlook.fr>
2022-02-27 17:27:12 +0100

    résolutions bugs et comptes admin

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const { ApolloServer } = require('apollo-server');
 const gql = require('graphql-tag');
 const mongoose = require('mongoose');
 
+const { MONGODB } = require('./config.js');
+
 const typeDefs = gql`
     type Query{
         sayHi: String!
@@ -10,18 +12,20 @@ const typeDefs = gql`
 
 const resolvers = {
     Query: {
-        sayHi: () => 'Hello world !!!!' 
+        sayHi: () => 'Hello world !!!!'
     }
 }
 
 const server = new ApolloServer({
     typeDefs,
     resolvers
-})
+});
 
-mongoose.connect()
-
-server.listen({ port: 5000 })
+mongoose.connect(MONGODB, { useNewUrlParser: true })
+    .then(() => {
+        console.log('MongoDB connected');
+        return server.listen({ port: 5000 });
+    })
     .then(res => {
         console.log(`Server running at ${res.url}`);
     })
